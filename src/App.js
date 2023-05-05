08e449b5f43afc611fe752e6917858e3510a1e15
Panayot Tolev <panayot.tolev@justeattakeaway.com>
2023-05-05 11:26:19 +0300

    React setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './assets/normalize.css';
+import './assets/styles.css';
+
+export default function App() {
+    return (
+        <div>
+            <h1>Our menu</h1>
+        </div>
+    );
+}
